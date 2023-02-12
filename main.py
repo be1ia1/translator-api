e6ed49e1a851353256ce1bf1a4bef046d26feaf2
Андрій Гула <gula@pr.km.ua>
2023-02-12 23:09:40 +0200

    add pandas part

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+df = pd.read_csv('dictionary.csv')
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/api/<word>')
-def toword(word):
-    return {'definition': word.upper(),
+def api(word):    
+    definition = df.loc[df['word'] == word]['definition'].squeeze()
+    return {'definition': definition,
             'word': word
     }
 
